cb6b131903e5509a0189b2b6bfa4c11de0ccbee1
Likithsreenivasa <118590713+Likithsreenivasa@users.noreply.github.com>
2023-07-23 17:08:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function getFormvalue() {
     //Write your code here
-
+	const first_name=document.getElementsByName('fname')[0].value;
+	const last_name=document.getElementsByName('lname')[0].value;
+	alert(`${first_name} ${last_name}`);
+	
 }
+const submit_button=document.getElementsByTagName('form')[2];
+submit_button.addEventListener('click', function event()
+{
+	event.preventDefault();
+	getFormvalue();
+	
+								   
+})
+
